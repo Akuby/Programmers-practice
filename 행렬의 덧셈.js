1b76b2b55a6198bf863926403cedf63a395d126b
Hyewon Ryu <akuby1997@gmail.com>
2022-01-27 23:19:16 +0900

    드디어 혼자 풀었다

--- 행렬의 덧셈.js
+++ 행렬의 덧셈.js
@@ -5,10 +5,10 @@ function solution(arr1, arr2) {
       answer.push([]);
       i++;
   }
-  for (let i = 0; i<arr1.length;i++){
-      for (let j = 0; j<arr1.length;i++){
+  for (let j = 0; j<arr1[0].length;j++){
+      for (let i = 0; i<arr1.length;i++){
           answer[i][j] = Number(arr1[i][j]) + Number(arr2[i][j]);
-      }
-    }
+        }
+  }
   return answer;
 }
