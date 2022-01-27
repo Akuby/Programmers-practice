77285372e180a6998b500046dcb59541324bd8c7
Hyewon Ryu <akuby1997@gmail.com>
2022-01-27 23:04:52 +0900

    2차원 배열 연습

---
+++ 행렬의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+  var answer = [];
+  let i = 0;
+  while (i <arr1.length){
+      answer.push([]);
+      i++;
+  }
+  for (let i = 0; i<arr1.length;i++){
+      for (let j = 0; j<arr1.length;i++){
+          answer[i][j] = Number(arr1[i][j]) + Number(arr2[i][j]);
+      }
+    }
+  return answer;
+}
